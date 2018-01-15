279d13393cfd9c066387da1bad2c9b3d88dbe8c3
Eric HERMENT <ericherment@outlook.com>
2018-01-15 11:35:00 +0100

    Working home screen PEL-15 PEL-17

--- Joueur/polyescape/src/pages/home/home.ts
+++ Joueur/polyescape/src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {NavController, ToastController} from 'ionic-angular';
+import {EscapeScreenPage} from "../escapeScreen/escapeScreen";
+import {TeamScreenPage} from "../teamScreen/teamScreen";
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,39 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  private inputName = '';
 
+  constructor(public navCtrl: NavController, public toastCtrl: ToastController) {
+
+  }
+
+  presentToast() {
+    let toast = this.toastCtrl.create({
+      message: 'Veuillez entrer un nom',
+      duration: 3000,
+      position: 'bottom'
+    });
+
+
+    toast.present();
+  }
+
+  verifyInputName() {
+    if (this.inputName.length == 0) {
+      this.presentToast();
+      return false;
+    }
+    return true;
+  }
+
+  goToEscapePage() {
+    if (this.verifyInputName())
+      this.navCtrl.push(EscapeScreenPage);
+  }
+
+  goToTeamPage() {
+    if (this.verifyInputName())
+      this.navCtrl.push(TeamScreenPage);
   }
 
 }
