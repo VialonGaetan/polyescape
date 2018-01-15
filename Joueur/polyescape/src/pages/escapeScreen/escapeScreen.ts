fb299286631f1e0850e15e69fd6fe39916b80fdb
Eric HERMENT <ericherment@outlook.com>
2018-01-15 14:37:42 +0100

    A game can be started PEL-17

--- Joueur/polyescape/src/pages/escapeScreen/escapeScreen.ts
+++ Joueur/polyescape/src/pages/escapeScreen/escapeScreen.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {EnigmePage} from "../enigme/enigme";
 
 @Component({
   selector: 'page-escape',
@@ -7,8 +8,17 @@ import { NavController } from 'ionic-angular';
 })
 export class EscapeScreenPage {
 
-  constructor(public navCtrl: NavController) {
+  escapeGames: any;
 
+  constructor(public navCtrl: NavController) {
+    this.escapeGames = [
+      'Escape Game 1',
+      'Escape Game 2',
+      'Escape Game 3'
+    ];
   }
 
+  startGame() {
+    this.navCtrl.push(EnigmePage);
+  }
 }
