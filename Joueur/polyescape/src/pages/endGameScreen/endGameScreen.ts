d8bc2df325ab9118e28a63e144eba0b61e2f3e40
Eric HERMENT <ericherment@outlook.com>
2018-01-24 23:53:14 +0100

    graph to view score

--- Joueur/polyescape/src/pages/endGameScreen/endGameScreen.ts
+++ Joueur/polyescape/src/pages/endGameScreen/endGameScreen.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+
 import {NavController, NavParams} from 'ionic-angular';
 import {HomePage} from "../home/home";
+import { Chart } from 'chart.js';
+import {ViewChildren, Component, ViewChild, ElementRef} from '@angular/core';
 
 @Component({
   selector: 'page-end',
@@ -8,10 +10,39 @@ import {HomePage} from "../home/home";
 })
 export class EndGameScreenPage {
 
+
+  @ViewChild('doughnutCanvas') doughnutCanvas;
+
   private score;
+  private doughnutChart: any;
+
 
   constructor(public navCtrl: NavController,public navParams:NavParams) {
     this.score = navParams.get("score");
+
+  }
+
+  ionViewDidLoad() {
+    this.doughnutChart = new Chart(this.doughnutCanvas.nativeElement, {
+
+      type: 'doughnut',
+      data: {
+        labels: ["Bonnes réponses", "Mauvaises réponses"],
+        datasets: [{
+          label: '# of Votes',
+          data: [12, 19],
+          backgroundColor: [
+            'rgba(243, 6, 6, 0.2)',
+            'rgba(6, 243, 6, 0.2)'
+          ],
+          hoverBackgroundColor: [
+            "#F30606",
+            "#06F306"
+          ]
+        }]
+      }
+
+    });
   }
 
   goToHomePage() {
