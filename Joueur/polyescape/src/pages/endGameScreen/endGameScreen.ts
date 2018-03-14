6be0293113004e5f42b681b09c1a8ac7c3e9e604
VialonGaetan <gaetan.vialon@gmail.com>
2018-03-14 09:01:53 +0100

    Suppression du maître du jeu et enseignant car mal ou non dev donc inutilisable

--- Joueur/polyescape/src/pages/endGameScreen/endGameScreen.ts
+++ Joueur/polyescape/src/pages/endGameScreen/endGameScreen.ts
@@ -1,7 +1,6 @@
 
 import {NavController, NavParams} from 'ionic-angular';
 import {HomePage} from "../home/home";
-import { Chart } from 'chart.js';
 import {ViewChildren, Component, ViewChild, ElementRef} from '@angular/core';
 
 @Component({
@@ -11,40 +10,13 @@ import {ViewChildren, Component, ViewChild, ElementRef} from '@angular/core';
 export class EndGameScreenPage {
 
 
-  @ViewChild('doughnutCanvas') doughnutCanvas;
-
-  private score;
-  private doughnutChart: any;
+  private score : number;
 
 
   constructor(public navCtrl: NavController,public navParams:NavParams) {
     this.score = navParams.get("score");
 
   }
-
-  ionViewDidLoad() {
-    this.doughnutChart = new Chart(this.doughnutCanvas.nativeElement, {
-
-      type: 'doughnut',
-      data: {
-        labels: ["Bonnes réponses", "Mauvaises réponses"],
-        datasets: [{
-          label: '# of Votes',
-          data: [12, 19],
-          backgroundColor: [
-            'rgba(243, 6, 6, 0.2)',
-            'rgba(6, 243, 6, 0.2)'
-          ],
-          hoverBackgroundColor: [
-            "#F30606",
-            "#06F306"
-          ]
-        }]
-      }
-
-    });
-  }
-
   goToHomePage() {
     this.navCtrl.setRoot(HomePage);
   }
